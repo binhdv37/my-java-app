8a6faa3aafdc50b69c8e6a94991df9b2a6d5093f
binhdv <binh.doan@sunteco.io>
2022-08-11 00:26:17 +0700

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,10 @@
 # syntax=docker/dockerfile:1
 
-FROM eclipse-temurin:17-jdk-jammy
-ARG TEST-ARG
-WORKDIR /app
-COPY .mvn/ .mvn
-COPY mvnw pom.xml ./
-RUN ./mvnw dependency:resolve
-COPY src ./src
-#ENV JDBC_URL 127.0.0.1:3306
-#ENV jdbc.url 127.0.0.1:3306
-ENV test=$TEST-ARG
-CMD ["./mvnw", "spring-boot:run"]
-
 FROM openjdk:8-jdk-alpine
 RUN addgroup -S spring && adduser -S spring -G spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
 ARG TEST-ARG
-COPY ${JAR_FILE} app.jar
-ENV test=$TEST-ARG
+COPY $JAR_FILE app.jar
+ENV jdbc.host=$TEST-ARG
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/myjavaapp/config/JpaConfig.java
+++ src/main/java/com/example/myjavaapp/config/JpaConfig.java
@@ -1,32 +1,35 @@
 package com.example.myjavaapp.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
 @Configuration
 public class JpaConfig {
 
-    @Value("${test}")
-    private String test;
+    @Value("${jdbc.host}")
+    private String jdbcHost;
 
     @PostConstruct
     public void test() {
         System.out.println("---------bdv-init-----------");
-        System.out.println(test);
+        System.out.println(jdbcHost);
     }
 
-//    @Bean
-//    public DataSource getDataSource() {
-//        System.out.println("env " + env);
-//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-//        dataSourceBuilder.url("jdbc:mysql://" + env + "/my_java_app");
-//        dataSourceBuilder.username("root");
-//        dataSourceBuilder.password("root");
-//        return dataSourceBuilder.build();
-//    }
+    @Bean
+    public DataSource getDataSource() {
+        System.out.println("---- env: " + jdbcHost);
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://" + jdbcHost + "/my_java_app");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("Root@123");
+        return dataSourceBuilder.build();
+    }
 
 
 
