6e3cdf1640a984df4337924c8adb9f9eef06717e
binhdv <binh.doan@sunteco.io>
2022-08-11 00:40:16 +0700

    chore: update code

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,14 @@ FROM openjdk:8-jdk-alpine
 RUN addgroup -S spring && adduser -S spring -G spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
-ARG TEST-ARG
+ARG ARG_DB_HOST
+ARG ARG_DB_USERNAME
+ARG ARG_DB_PASSWORD
 COPY $JAR_FILE app.jar
-ENV jdbc.host=$TEST-ARG
+ENV DB_HOST=$ARG_DB_HOST
+ENV DB_USERNAME=$ARG_DB_USERNAME
+ENV DB_PASSWORD=$ARG_DB_PASSWORD
 ENTRYPOINT ["java","-jar","/app.jar"]
+
+# ARG : use to set variable in build time
+# ENV: environment of app, pass in build time or run time
--- src/main/java/com/example/myjavaapp/config/JpaConfig.java
+++ src/main/java/com/example/myjavaapp/config/JpaConfig.java
@@ -11,23 +11,31 @@ import javax.sql.DataSource;
 @Configuration
 public class JpaConfig {
 
-    @Value("${jdbc.host}")
-    private String jdbcHost;
+    @Value("${DB.HOST}")
+    private String dbHost;
+
+    @Value("${DB.USERNAME}")
+    private String dbUsername;
+
+    @Value("${DB.PASSWORD}")
+    private String dbPassword;
 
     @PostConstruct
     public void test() {
         System.out.println("---------bdv-init-----------");
-        System.out.println(jdbcHost);
+        System.out.println("DB_HOST: " + dbHost);
+        System.out.println("DB_USERNAME: " + dbUsername);
+        System.out.println("DB_PASSWORD: " + dbPassword);
     }
 
     @Bean
     public DataSource getDataSource() {
-        System.out.println("---- env: " + jdbcHost);
+        System.out.println("---- env: " + dbHost);
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://" + jdbcHost + "/my_java_app");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("Root@123");
+        dataSourceBuilder.url("jdbc:mysql://" + dbHost + "/my_java_app");
+        dataSourceBuilder.username(dbUsername);
+        dataSourceBuilder.password(dbPassword);
         return dataSourceBuilder.build();
     }
 
