0126633137fc6d3ef6f32d8506efa78d0952cc83
binhdv <binh.doan@sunteco.io>
2022-08-10 18:09:21 +0700

    asd

--- src/main/java/com/example/myjavaapp/config/JpaConfig.java
+++ src/main/java/com/example/myjavaapp/config/JpaConfig.java
@@ -1,28 +1,33 @@
 package com.example.myjavaapp.config;
 
-import org.hibernate.cfg.Environment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class JpaConfig {
-    @Value("${jdbc.url}")
-    private String env;
 
-    @Bean
-    public DataSource getDataSource() {
-        System.out.println("env " + env);
-        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://" + env + "/my_java_app");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("root");
-        return dataSourceBuilder.build();
+    @Value("${test}")
+    private String test;
+
+    @PostConstruct
+    public void test() {
+        System.out.println("---------bdv-init-----------");
+        System.out.println(test);
     }
+
+//    @Bean
+//    public DataSource getDataSource() {
+//        System.out.println("env " + env);
+//        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSourceBuilder.url("jdbc:mysql://" + env + "/my_java_app");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("root");
+//        return dataSourceBuilder.build();
+//    }
+
+
 
 }
